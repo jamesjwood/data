4b6648351ffddf671dcbd5d6589a9fafa14c779a
jamesjwood <jamesjwood@msn.com>
2013-11-12 15:56:55 +0000

    from imac

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,4 @@
-var getWatchers = require('getwatchers');
+var getWatchers = require('getWatchers');
 
 module.exports = function(grunt) {
   "use strict";
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
           debounceDelay: 5000,
           interrupt: true
         },
-        files: getWatchers(),
+        files: getWatchers(require('./package.json')),
         tasks: ['default']
       }
     },
@@ -31,33 +31,43 @@ module.exports = function(grunt) {
 shell: {
       makeStage: {
         command: 'rm -rf stage; mkdir stage',
-        stdout: true,
-        stderr: true,
-        failOnError: true
+        options:{
+          stdout: true,
+          stderr: true,
+          failOnError: true
+        }
       },
       makeLib: {
         command: 'rm -rf lib; mkdir lib',
-        stdout: true,
-        stderr: true,
-        failOnError: true
+        options:{
+          stdout: true,
+          stderr: true,
+          failOnError: true
+        }
       },
       browserify:{
         command: 'node ./node_modules/browserify/bin/cmd.js  --debug -o ./stage/test.js -i domain -e ./test.js;',
-        stdout: true,
-        stderr: true,
-        failOnError: true
+        options:{
+          stdout: true,
+          stderr: true,
+          failOnError: true
+        }
       },
       buildPouchDBClient:{
         command: 'cd node_modules/pouchdb; npm install; grunt;',
-        stdout: true,
-        stderr: true,
-        failOnError: true
+        options:{
+          stdout: true,
+          stderr: true,
+          failOnError: true
+        }
       },
       copyPouch:{
         command: 'cp -av node_modules/pouchdb/dist/pouchdb-nightly.min.js lib/pouch.min.js; cp -av node_modules/pouchdb/dist/pouchdb-nightly.js lib/pouch.js;',
-        stdout: true,
-        stderr: true,
-        failOnError: true
+        options:{
+          stdout: true,
+          stderr: true,
+          failOnError: true
+        }
       }
     },
     karma: {
